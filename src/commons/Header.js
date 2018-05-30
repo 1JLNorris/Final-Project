c67b5648107651fbb48c0a2c1f36054309b89cb8
Jugena <jugenanorris123@gmail.com>
2018-05-29 17:01:18 -0700

    added files

---
+++ src/commons/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => (
+  <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="#">Navbar</a>
+</nav>
+)
+
+export default Header
